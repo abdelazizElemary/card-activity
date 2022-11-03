bb12d8892f57fb1693234fa9c8fed45db7125f34
VladStarostenko <vlad.starostenko@data-lake.co>
2022-11-03 15:56:12 +0100

    Implement liquidity modal with mocked data

--- src/components/widgets/provideLiquidity/ProvideLiquidityModal.tsx
+++ src/components/widgets/provideLiquidity/ProvideLiquidityModal.tsx
@@ -1,4 +1,8 @@
+import { Button } from '../../button/Button';
+import { GradientButton } from '../../button/gradient/GradientButton';
 import ReactModal from 'react-modal';
+import { TokenInput } from './TokenInput';
+import cancelIcon from './../../../assets/icons/cancel-icon.svg';
 
 type Props = {
     isOpen: boolean;
@@ -7,9 +11,11 @@ type Props = {
 
 const customStyles = {
     overlay: {
-        background: 'rgba(0, 0, 0, 0.4)',
+        background: 'rgba(0, 0, 0, 0.8)',
     },
     content: {
+        background: 'transparent',
+        border: 'none',
         top: '50%',
         left: '50%',
         right: 'auto',
@@ -22,6 +28,12 @@ const customStyles = {
 ReactModal.setAppElement('#root');
 
 export const ProvideLiquidityModal = ({ isOpen, closeModal }: Props) => {
+    const onApproveClick = () => {
+        console.log('approve');
+    };
+    const onProvideLiquidityClick = () => {
+        console.log('provide');
+    };
     return (
         <ReactModal
             isOpen={isOpen}
@@ -29,18 +41,50 @@ export const ProvideLiquidityModal = ({ isOpen, closeModal }: Props) => {
             contentLabel="Example Modal"
         >
             <div className="flex flex-col">
-                <div className="flex justify-end">
-                    <button onClick={closeModal}>close</button>
-                </div>
-                <div className="flex flex-col">
-                    <div>PROVIDE LIQUIDITY</div>
-                    <div className="flex flex-col">
-                        <div>USDT</div>
-                        <div>LAKE</div>
+                <div className="flex justify-end items-center mb-6">
+                    <span className="text-sm tracking-[.1em] mr-2 text-gray-500">
+                        CLOSE
+                    </span>
+                    <div className="w-8 h-8 flex justify-center items-center rounded-[32px] border border-gray-500 p-1">
+                        <img
+                            className="cursor-pointer"
+                            src={cancelIcon}
+                            onClick={closeModal}
+                            alt="copy"
+                        ></img>
                     </div>
-                    <div className="flex flex-col">
-                        <div>APPROVE</div>
-                        <div>PROVIDE LIQUIDITY</div>
+                </div>
+                <div className="flex flex-col rounded-[32px] border border-gray-500 p-8 bg-black-800">
+                    <div className="font-kanit-medium color-gray-gradient text-shadow text-xl tracking-[.12em] text-center mb-4">
+                        PROVIDE LIQUIDITY
+                    </div>
+                    <div className="flex flex-col w-[20vw]">
+                        <TokenInput
+                            tokenSymbol="USDT"
+                            tokenAmount={1260}
+                            tokenPrice={1}
+                        />
+                        <TokenInput
+                            tokenSymbol="LAKE"
+                            tokenAmount={2700}
+                            tokenPrice={0.47}
+                        />
+                    </div>
+                    <div className="flex flex-col mt-8 items-center">
+                        <GradientButton
+                            size="medium"
+                            disabled={false}
+                            text="APPROVE LAKE"
+                            onClick={onApproveClick}
+                        />
+                        <div className="mt-6 mb-2">
+                            <Button
+                                size="medium"
+                                disabled={false}
+                                text="PROVIDE LIQUIDITY"
+                                onClick={onProvideLiquidityClick}
+                            />
+                        </div>
                     </div>
                 </div>
             </div>
---
+++ src/components/widgets/provideLiquidity/TokenInput.tsx
@@ -0,0 +1,54 @@
+import { colors } from '../../../constants/colors';
+import { formatValue } from '../../../utils/formatValue';
+import lakeLogo from './../../../assets/icons/lake-logo.svg';
+import styled from 'styled-components';
+import usdtLogo from './../../../assets/icons/usdt-logo.svg';
+
+type Props = {
+    tokenSymbol: string;
+    tokenAmount: number;
+    tokenPrice: number;
+};
+
+export const TokenInput = ({ tokenSymbol, tokenAmount, tokenPrice }: Props) => {
+    const onMaxClick = () => {
+        console.log('max');
+    };
+    return (
+        <InputContainer className="w-full h-[7rem] flex justify-between pl-8 pr-2 pt-2 my-4">
+            <div className="w-1/4 flex flex-col justify-center items-center mr-4">
+                <span className="text-sm tracking-[.1em] text-gray-300 mb-2">
+                    {tokenSymbol}
+                </span>
+                <img
+                    className="w-[2.5rem] h-[2.5rem]"
+                    src={tokenSymbol === 'USDT' ? usdtLogo : lakeLogo}
+                    alt="chart"
+                ></img>
+            </div>
+            <div className="w-full flex flex-col items-end">
+                <button
+                    className="border border-gray-500 rounded-[32px] px-2 "
+                    onClick={onMaxClick}
+                >
+                    <span>MAX</span>
+                </button>
+                <div className="flex flex-col items-center mt-2 mr-4">
+                    <span className="font-kanit-medium color-gradient text-2xl font-bold">
+                        {formatValue(tokenAmount, tokenSymbol, 0)}
+                    </span>
+                    <span className="font-kanit-medium whitespace-nowrap text-xs tracking-[.12em]">
+                        = {formatValue(tokenAmount * tokenPrice, '$', 0)}
+                    </span>
+                </div>
+            </div>
+        </InputContainer>
+    );
+};
+
+const InputContainer = styled.div`
+    box-shadow: inset 1px 1px 1px rgba(68, 68, 68, 0.05),
+        inset -1px -1px 4px rgba(134, 134, 134, 0.12);
+    border-radius: 20px;
+    background: ${colors.black[600]};
+`;
